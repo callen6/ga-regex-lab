27f7118bfa8f505ba5d8eb5a1506003ba3070e10
Cori Allen <coriallen@gmail.com>
2013-10-02 22:40:54 -0400

    Adds working solutions to regex lab.

--- main.rb
+++ main.rb
@@ -23,14 +23,16 @@ non_mit_array << non_mit_ip_addresses($source_text)
 
 
 def valid_phone_numbers(numbers)
-	numbers.scan(/[^-][^1]\d{2}-\d{3}-\d{4}/)
-	numbers.scan(/[^-]\d{3}-\d{4}/)  
+	numbers.scan(/\d{3}?\d?-?\d{3}-\d{4}/).slice(1..3)
+		#/[^-]\d{3}-\d{4}/  
+		#/\d{3}?\d?-?\d{3}-\d{4}/
+		#/[^-][^1]\d{2}-\d{3}-\d{4}/
 end
 valid_numbers_array = []
 valid_numbers_array << valid_phone_numbers($source_text)
 
 def area_codes(codes)
-	codes.scan(/\d{5}/)
+	codes.scan(/[6-8][0-1]0?7?/).slice(1,2)
 end
 valid_area_codes_array = []
 valid_area_codes_array << area_codes($source_text)
@@ -47,7 +49,12 @@ end
 valid_zip_code_array =
 
 def hex_colors(hex) 
-	hex.scan(/#\b\w{1,6}\b/)
+	# hex.scan(/#\b\w{1,6}\b/)
+	# hex.scan(/\b\w{6}\b\s?\./)
+	# hex.scan(/#\w{6}/).slice(0..2)
+	hex.scan(/\h{6}/).slice(1,3)
 end
 valid_hex_array = []
 valid_hex_array << hex_colors($source_text)
+
+
